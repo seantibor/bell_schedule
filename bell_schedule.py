4c636977b174dc4d292c60115cbc4cfca8bf64ca
Sean Tibor <sean.tibor@pinecrest.edu>
2019-05-16 09:48:52 -0400

    passing all tests. using Arrow

--- bell_schedule.py
+++ bell_schedule.py
@@ -1,19 +1,22 @@
-from collections import namedtuple
+from collections import namedtuple, OrderedDict
 import datetime as dt
 import csv
+import arrow
 
 Period = namedtuple('Period', ['name','start_time','end_time'])
+datetime_format = 'YYYY-M-D H:mm'
 
 class BellSchedule():
     
-    def __init__(self):
-        self.periods = {}
+    def __init__(self, tz: dt.tzinfo=None):
+        self.periods = OrderedDict()
+        self.tz = tz
 
     def add_period(self, period_name: str=None, start_time: dt.datetime=None, end_time: dt.datetime=None, period: Period=None) -> None:
         if period:
             self.periods[period.name] = period
         else:
-            self.periods[period_name] = Period(period_name, start_time, end_time)
+            self.periods[period_name] = Period(period_name, arrow.Arrow.fromdatetime(start_time, tzinfo=self.tz), arrow.Arrow.fromdatetime(end_time, tzinfo=self.tz))
 
     def remove_period(self, period_name: str=None, period_tup: Period=None) -> None:
         if period_tup:
@@ -25,27 +28,28 @@ class BellSchedule():
         return self.periods[period_name]
 
     def as_list_of_dicts(self) -> list:
-        return [{'period_name': period.name, 'start_time': period.start_time, 'end_time': period.end_time} for period in self.periods.values()]
+        return [period._asdict() for period in self.periods.values()]
 
     @classmethod
-    def from_csv(cls, filename: str):
+    def from_csv(cls, filename: str, tz: dt.tzinfo=None):
         bell_schedule = BellSchedule()
         with open(filename) as infile:
             bellreader = csv.DictReader(infile)
             for row in bellreader:
-                bell_schedule.add_period(row['period_name'], dt.datetime.strptime(row['start_time'], '%Y-%m-%d %H:%M'), dt.datetime.strptime(row['end_time'], '%Y-%m-%d %H:%M'))
+                bell_schedule.add_period(row['name'], arrow.get(row['start_time'], datetime_format), arrow.get(row['end_time'], datetime_format))
 
         return bell_schedule
 
     def to_csv(self, filename: str) -> None:
         with open(filename, 'w') as outfile:
-            fieldnames = ['period_name', 'start_time', 'end_time']
+            fieldnames = ['name', 'start_time', 'end_time']
             bellwriter = csv.DictWriter(outfile, fieldnames=fieldnames)
             bellwriter.writeheader()
             for row in self.as_list_of_dicts():
                 bellwriter.writerow(row)
 
     def current_period(self, current_time=dt.datetime.now()):
+        current_time = arrow.Arrow.fromdatetime(current_time, tzinfo=self.tz)
         for period in self.periods.values():
             if period.start_time < current_time and current_time < period.end_time:
                 return period
--- test_bell_schedule.py
+++ test_bell_schedule.py
@@ -1,14 +1,16 @@
 from bell_schedule import Period, BellSchedule
 from datetime import datetime, timedelta
-import pytest
+import pytest, arrow
+
+timezone = 'US/Eastern'
 
 @pytest.fixture(scope="module")
 def pc_bellschedule():
-    return BellSchedule.from_csv('test_input.csv')
+    return BellSchedule.from_csv('test_input.csv', tz=timezone)
 
 def test_create_period():
-    start_time = datetime(2019, 5, 12, 8, 20)
-    end_time = datetime(2019, 5, 12, 9, 2)
+    start_time = arrow.Arrow(2019, 5, 12, 8, 20, tzinfo=timezone)
+    end_time = arrow.Arrow(2019, 5, 12, 9, 2, tzinfo=timezone)
     period = Period(name='1', start_time=start_time, end_time=end_time)
     assert period.name == '1'
     assert period.start_time == start_time
@@ -21,7 +23,7 @@ def test_add_period_by_attributes(pc_bellschedule):
 
 def test_add_period_by_namedtuple(pc_bellschedule):
     start_count = len(pc_bellschedule.periods)
-    pc_bellschedule.add_period(period=Period('Y', datetime.now(), datetime.now()))
+    pc_bellschedule.add_period(period=Period('Y', arrow.now(), arrow.now()))
     assert len(pc_bellschedule.periods) == start_count + 1
 
 def test_schedule_to_csv(pc_bellschedule):
@@ -41,7 +43,7 @@ def test_current_period(pc_bellschedule):
 
 def test_no_current_period(pc_bellschedule):
     test_time = datetime(2019, 5, 14, 18, 25)
-    period = pc_bellschedule.current_period(test_time)
+    period = pc_bellschedule.current_period(current_time=test_time)
     assert period is None
 
 def test_remove_period_by_name(pc_bellschedule):
